9f0bfc5ec5fb6829b3dc9a1d98672c98c3192340
Rafael Augusto <103936985+rafael-tecinternet@users.noreply.github.com>
2022-10-14 09:11:30 -0300

    criação das rotas browserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Cabecalho from "./components/layout/Cabecalho";
 import Rodape from "./components/layout/Rodape";
 import Home from "./pages/Home";
@@ -7,11 +8,21 @@ import Produtos from "./pages/Produtos";
 const App = () => {
   return (
     <>
-      <Cabecalho />
-      <Home />
-      <Produtos />
-      <Servicos />
-      <Rodape />
+      <BrowserRouter>
+        <Cabecalho />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/produtos">
+            <Produtos />
+          </Route>
+          <Route path="/servicos">
+            <Servicos />
+          </Route>
+        </Switch>
+        <Rodape />
+      </BrowserRouter>
     </>
   );
 };
